9d806d2ca78a5583072cd3b6f14201c2e463b2ca
hailMeh <Sultanhabibi@mail.ru>
2021-02-24 10:55:39 +0300

    Add all Calculator Files

---
+++ index.js
@@ -0,0 +1,31 @@
+calc.start();
+
+// var arrayOfOperations = document.getElementsByClassName("button-operation");
+// var firstInputEl = document.getElementById("firstInput");
+// var secondInputEl = document.getElementById("secondInput");
+
+// function calculating(operator) {
+//   var firstInput = Number(firstInputEl.value);
+//   var secondInput = Number(secondInputEl.value);
+//   if (operator === "+") {
+//     var result = firstInput + secondInput;
+//   } else if (operator === "-") {
+//     var result = firstInput - secondInput;
+//   } else if (operator === "*") {
+//     var result = firstInput * secondInput;
+//   } else if (operator === "/") {
+//     var result = firstInput / secondInput;
+//   } else {
+//     window.alert("Operator is unknown");
+//   }
+//   window.alert(result);
+// }
+// function operationOnButtonClick(eventObject) {
+//   var kindOfOperation = eventObject.currentTarget;
+//   var operator = kindOfOperation.innerHTML;
+//   calculating(operator);
+// }
+// for (let i = 0; i < arrayOfOperations.length; i++) {
+//   const button = arrayOfOperations[i];
+//   button.addEventListener("click", operationOnButtonClick);
+// }
